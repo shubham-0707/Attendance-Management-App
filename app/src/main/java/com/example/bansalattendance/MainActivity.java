8845eae6d03d2d11fc09252c66bc1a277548ebed
Shubham Singh <shubhamsinghlmp578@gmail.com>
2022-02-13 19:44:27 +0530

    First Commit

---
+++ app/src/main/java/com/example/bansalattendance/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.bansalattendance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this , HomePage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
